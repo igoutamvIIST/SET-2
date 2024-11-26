ad45efc732b9e1bccad2a4336fb7d76bd346bcc1
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-26 12:01:25 +0530

    Question2.c

---
+++ Question2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {7, 2, 6, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i_count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                i_count++;
+            }
+        }
+    }
+
+    printf("Inversion count: %d\n", i_count);
+
+    return 0;
+}
