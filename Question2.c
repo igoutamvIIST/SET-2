d994f49564e615d23f4e72284b97faab2dd05dab
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 11:56:57 +0530

    Update Question2.c

--- Question2.c
+++ Question2.c
@@ -1,3 +1,11 @@
+/*
+Given an integer array arr[] of size n, the task is to find the count inversions of the given array.
+Two array elements arr[i] and arr[j] form an inversion if arr[i] > arr[j] and i < j.
+Input: arr[] = {7, 2, 6, 3}
+Output: 4
+Explanation: Given array has 4 inversions: (7, 2), (7, 6), (7, 3), (6, 3)
+*/
+
 #include <stdio.h>
 int main() {
     int arr[] = {7, 2, 6, 3};
