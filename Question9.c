3a1f99c0cf91d6d50c496409e602362a1d51569f
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 12:05:42 +0530

    Update Question9.c

--- Question9.c
+++ Question9.c
@@ -1,1 +1,9 @@
-Question9.c
+/*
+Given an integer array, find the maximum length subarray having a given sum.(USE
+HASHMAP Concept)
+
+Input:nums[] = { 5, 6, -5, 5, 3, 5, 3, -2, 0 }target = 8
+Output:{ -5, 5, 3, 5 }
+Subarrays with sum 8 are { -5, 5, 3, 5 },{ 3, 5 },{ 5, 3 }
+The longest subarray is { -5, 5, 3, 5 } having length 4.
+*/
