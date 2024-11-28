2db9745f28b074595e3dadf6ec69f47ec1e73a66
Goutam Vishwakarma IIST <145545968+igoutamvIIST@users.noreply.github.com>
2024-11-28 12:02:22 +0530

    Update Question6.c

--- Question6.c
+++ Question6.c
@@ -1,1 +1,11 @@
-Question6.c
+/*
+Given a sorted array A (sorted in ascending order), having N integers, find if there exists any
+pair of elements (A[i], A[j]) such that their sum is equal to X.
+        a)Using Naive Approach
+        b)Using Two Pointer Technique
+Input: A = [1, 2, 4, 5, 7, 11]
+       N = 6
+       X = 9
+Expected Output: Yes
+(Explanation: The pair (2, 7) sums to 9.)
+*/
